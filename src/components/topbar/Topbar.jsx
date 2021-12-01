4a81c871889333b02e9d78b4c269e2de1a736a6e
Hoang Duong Nguyen <hoangduongnguyen.tech@gmail.com>
2021-12-01 11:01:40 +0700

    base

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./topbar.css";
+import { NotificationsNone, Language, Settings } from "@material-ui/icons";
+
+export default function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">DOTS.admin</span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsNone />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Language />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Settings />
+          </div>
+          <img src="https://res.cloudinary.com/dlyf81nhk/image/upload/v1638196027/DOTS%20-Web/DOTS_web_loogoArtboard_2_bffegc.jpg"
+          alt="" className="topAvatar" />
+        </div>
+      </div>
+    </div>
+  );
+}
